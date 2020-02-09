a707d9ca21b7fec84ca9b7ce74be60653c2b999f
aaab1141 <aaron.aboaf@gmail.com>
2020-02-09 10:42:49 -0700

    started HW2

---
+++ HW2_mainshell.m
@@ -0,0 +1,24 @@
+% % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % 
+% This is the main shell script for ASEN 5218 Homework #2. It facilitates
+% running the problems in the homework. To check this homework, the only 
+% thing that is required is to run this script.
+% % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % %
+clear
+close all
+
+% Question 1 & 2 are derivations
+
+% Question 3
+% Looking to plot the linear density of both truss designs
+E = 110e9; %Pa
+rho = 1500; %kg/m^3
+thickness_min = 0.5e-3; %m
+num_longerons = [3 6 12];
+radius = [0.1 0.3 1]; %m
+loadP_min = 10; %N
+loadP_max = 10000; %N
+
+
+% Question 4 is a derivation
+
+% Question 5
