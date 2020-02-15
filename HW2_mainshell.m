e9eb6abe560335702ce1f3f9820ff75d04af17e7
aaab1141 <aaron.aboaf@gmail.com>
2020-02-15 15:05:33 -0700

    q2 plot added

--- HW2_mainshell.m
+++ HW2_mainshell.m
@@ -14,44 +14,50 @@ num_longerons = 2:1:7;
 num_longerons = num_longerons';
 for n = 2:1:7
     temp = (sin(pi/n)/sqrt(n)) * ((10*cot(theta)) + sqrt(10)./sin(theta).*1./cos(theta));
-    [value_of_mu(n-1),i] = min(temp);
-    optimal_theta(n-1,1) = theta(i);
+    [value_of_mu.cyl(n-1),i] = min(temp);
+    optimal_theta.cyl(n-1,1) = theta(i);
     plot(theta,temp)
 end
 % table of min thetas
-table(num_longerons,optimal_theta)
+table(num_longerons,optimal_theta.cyl)
 
 %finish plot
-optimal_theta = [optimal_theta;optimal_theta(1)];
-value_of_mu = [value_of_mu,50];
-plot(optimal_theta',value_of_mu,'r','linewidth',2);
+optimal_theta.cyl = [optimal_theta.cyl;optimal_theta.cyl(1)];
+value_of_mu.cyl = [value_of_mu.cyl,50];
+plot(optimal_theta.cyl',value_of_mu.cyl,'r','linewidth',2);
 axis([0,pi/2,0,50]); grid on
-title('$\mu_n \mu_{\theta}$ for Increasing Number of Longerons','interpreter','latex')
+title('Solid Cylinder $\mu_n \mu_{\theta}$ for Increasing Number of Longerons','interpreter','latex')
 ylabel('\mu_n \mu_{\theta}');xlabel('\theta, radians')
 legend('n = 2','n = 3','n = 4','n = 5','n = 6','n = 7','Minimum \theta','location','north')
-h = text(optimal_theta(1)+.03,30,['Minimum \theta = ',num2str(optimal_theta(1)*180/pi),'^o'],'horizontalalignment','center');
+h = text(optimal_theta.cyl(1)+.03,30,['Minimum \theta = ',num2str(optimal_theta.cyl(1)*180/pi),'^o'],'horizontalalignment','center');
 set(h,'Rotation',90)
  
-% % Question 2
-% figure
-% hold on
-% theta = pi/40:.01:pi/2;
-% num_longerons = 2:1:7;
-% num_longerons = num_longerons';
-% for n = 2:1:7
-%     temp = sin(pi/n)^(2/3)/n^(1/3) * (5*2^(2/3)*cot(theta) + 
-%     [value_of_mu(n-1),i] = min(temp);
-%     optimal_theta(n-1,1) = theta(i);
-%     plot(theta,temp)
-% end
-% plot(optimal_theta',value_of_mu,'r','linewidth',2);
-% axis([0,pi/2,0,50]); grid on
-% title('$\mu_n \mu_{\theta}$ for Increasing Number of Longerons','interpreter','latex')
-% ylabel('\mu_n \mu_{\theta}');xlabel('\theta, radians')
-% legend('n = 2','n = 3','n = 4','n = 5','n = 6','n = 7','Minimum \theta','location','north')
-% 
-% % table of min thetas
-% table(num_longerons,optimal_theta)
+% Question 2
+figure
+hold on
+theta = pi/40:.01:pi/2;
+num_longerons = 2:1:7;
+num_longerons = num_longerons';
+for n = 2:1:7
+    temp = sin(pi/n)^(2/3)/n^(1/3) * (5*2^(2/3)*cot(theta) + (2^(1/3)*5^(2/3))./(sin(theta).^(2/3).*cos(theta)));
+    [value_of_mu.tube(n-1),i] = min(temp);
+    optimal_theta.tube(n-1,1) = theta(i);
+    plot(theta,temp)
+end
+% table of min thetas
+table(num_longerons,optimal_theta.tube)
+
+%finish plot
+optimal_theta.tube = [optimal_theta.tube;optimal_theta.tube(1)];
+value_of_mu.tube = [value_of_mu.tube,50];
+plot(optimal_theta.tube',value_of_mu.tube,'r','linewidth',2);
+axis([0,pi/2,0,50]); grid on
+title('Solid Cylinder $\mu_n \mu_{\theta}$ for Increasing Number of Longerons','interpreter','latex')
+ylabel('\mu_n \mu_{\theta}');xlabel('\theta, radians')
+legend('n = 2','n = 3','n = 4','n = 5','n = 6','n = 7','Minimum \theta','location','north')
+h = text(optimal_theta.tube(1)+.03,30,['Minimum \theta = ',num2str(optimal_theta.tube(1)*180/pi),'^o'],'horizontalalignment','center');
+set(h,'Rotation',90)
+ 
 
 % Question 3
 % Looking to plot the linear density of both truss designs
