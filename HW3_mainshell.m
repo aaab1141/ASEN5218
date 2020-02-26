f8efa4e388f7c681a2d39793c95a5f51cb0af3d5
aaab1141 <aaron.aboaf@gmail.com>
2020-02-26 10:25:34 -0700

    adding HW3

---
+++ HW3_mainshell.m
@@ -0,0 +1,9 @@
+% % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % 
+% This is the main shell script for ASEN 5218 Homework #3. It facilitates
+% running the problems in the homework. To check this homework, the only 
+% thing that is required is to run this script.
+% % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % %
+clear
+close all
+
+
