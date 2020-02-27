4edf78e4e378f940a1997db224dae0b8eee363ec
aaab1141 <aaron.aboaf@gmail.com>
2020-02-26 21:53:54 -0700

    plots for Q1

--- HW3_mainshell.m
+++ HW3_mainshell.m
@@ -6,4 +6,18 @@
 clear
 close all
 
+% Question 1
+theta = 0:0.01:2*pi;
+%   a) triangle-on-triangle lattice
+R = 2/3*(cos(theta).^4 + sin(theta).^4);
+figure
+polarplot(theta,R)
+title('Bending Efficiency: Square-on-Square 2 Layer Lattice')
 
+%   b) square-on-square lattice
+R = 3/4 ; R = repelem(R,length(theta));
+figure
+polarplot(theta,R)
+title('Bending Efficiency: Triangle-on-Triangle 2 Layer Lattice')
+
+% Question 2
