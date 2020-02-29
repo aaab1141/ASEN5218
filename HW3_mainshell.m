f16af4b6abac9db8bfa8949cfc74b7c2962cdbde
aaab1141 <aaron.aboaf@gmail.com>
2020-02-28 20:09:44 -0700

    plots for Q2

--- HW3_mainshell.m
+++ HW3_mainshell.m
@@ -27,15 +27,26 @@ d = 20; %m
 A = 0.005^2*pi; %m Seems like a reasonable value since it's not given
 Nr = linspace(3,20,100);
 eta = linspace(0.05,0.5,100);
-s = zeros(length(Nr),length(eta));
+nf = zeros(length(Nr),length(eta));
 for i = 1:length(Nr)
     for j = 1:length(eta)
-        s(i,j) = natfreq(EE,rho,A,Nr(i),d,eta(j));
+        nf(i,j) = natfreq(EE,rho,A,Nr(i),d,eta(j));
     end
 end
+[Nr,eta] = meshgrid(Nr,eta);
 
+figure
+contour(Nr,eta,nf)
+xlabel('Number of Rings, N_r');ylabel('Parasitc Mass Ratio, \eta')
+colorbar; grid on
+title('Contour Plot of Natural Frequency')
 
+figure
+surf(Nr,eta,nf)
+xlabel('Number of Rings, N_r');ylabel('Parasitc Mass Ratio, \eta');zlabel('Natural Frequency, Hz')
+title('Surface Plot of Natural Frequency')
 
+% Question 3
 
 % Functions:
 function [f] = natfreq(EE,rho,A,Nr,d,eta)
