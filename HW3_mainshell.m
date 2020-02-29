9ced3429fe54e60feb5e90814e0ec782637831a9
aaab1141 <aaron.aboaf@gmail.com>
2020-02-28 19:57:17 -0700

    added function for Q2

--- HW3_mainshell.m
+++ HW3_mainshell.m
@@ -21,3 +21,23 @@ polarplot(theta,R)
 title('Bending Efficiency: Triangle-on-Triangle 2 Layer Lattice')
 
 % Question 2
+EE = 105e9; %Pa
+rho = .0015/100^3; %kg/m^3
+d = 20; %m
+A = 0.005^2*pi; %m Seems like a reasonable value since it's not given
+Nr = linspace(3,20,100);
+eta = linspace(0.05,0.5,100);
+s = zeros(length(Nr),length(eta));
+for i = 1:length(Nr)
+    for j = 1:length(eta)
+        s(i,j) = natfreq(EE,rho,A,Nr(i),d,eta(j));
+    end
+end
+
+
+
+
+% Functions:
+function [f] = natfreq(EE,rho,A,Nr,d,eta)
+f = (3^0.25*(1)*sqrt(EE)*sqrt(d)) / (3*pi*sqrt(192)*(2*Nr-1)^(5/2)*sqrt(A)*sqrt(rho)*sqrt(1+eta));
+end
