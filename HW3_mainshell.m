d76f56a925e798879b01d96197581ef3c08804aa
aaab1141 <aaron.aboaf@gmail.com>
2020-03-01 15:12:28 -0700

    q4 plots

--- HW3_mainshell.m
+++ HW3_mainshell.m
@@ -25,8 +25,8 @@ EE = 105e9; %Pa
 rho = .0015/100^3; %kg/m^3
 d = 20; %m
 A = 0.005^2*pi; %m Seems like a reasonable value since it's not given
-Nr = linspace(3,20,100);
-eta = linspace(0.05,0.5,100);
+Nr = linspace(3,20,50);
+eta = linspace(0.05,0.5,50);
 nf = zeros(length(Nr),length(eta));
 for i = 1:length(Nr)
     for j = 1:length(eta)
@@ -47,8 +47,37 @@ xlabel('Number of Rings, N_r');ylabel('Parasitc Mass Ratio, \eta');zlabel('Natur
 title('Surface Plot of Natural Frequency')
 
 % Question 3
+% assume same values as Q2 for E and A
+theta_0 = pi/4;
+% choose reasonable values for d (thickness), l (length of bars), and r_0
+d = .01; %10cm
+l = .01; %10cm
+r_0 = 1;
+oor = linspace(1/100,10,50);
+theta = linspace(0,pi,50);
+for i = 1:length(oor)
+    for j = 1:length(theta)
+        U(i,j) = strainenergy(d,A,EE,l,theta(i),theta_0,oor(j),r_0);
+    end
+end
+[oor,theta] = meshgrid(oor,theta);
+
+figure
+contour(oor,theta,U)
+xlabel('1/r');ylabel('\theta, radians')
+title('Countour Plot of Strain Energy')
+colorbar; grid on
+
+figure
+surf(oor,theta,U)
+xlabel('1/r');ylabel('\theta, radians');zlabel('Strain Energy, J')
+title('Surface Plot of Strain Energy')
 
 % Functions:
 function [f] = natfreq(EE,rho,A,Nr,d,eta)
 f = (3^0.25*(1)*sqrt(EE)*sqrt(d)) / (3*pi*sqrt(192)*(2*Nr+1)^(5/2)*sqrt(A)*sqrt(rho)*sqrt(1+eta));
+end
+
+function [u] = strainenergy(d,A,EE,l,theta,theta_0,oor,r_0)
+u = d^2*A*EE/(4*l)*(oor^(2)*(cos(theta)^4+cos(theta+pi/2)^4) - r_0^(-2)*(cos(theta_0)^4+cos(theta_0+pi/2)^4));
 end
