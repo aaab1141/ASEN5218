2d604242fb32117c19d3d722fc1d88a88a6d1ed4
aaab1141 <aaron.aboaf@gmail.com>
2020-04-23 13:37:05 -0600

    comments updated

--- save_fig_png.m
+++ save_fig_png.m
@@ -2,9 +2,10 @@ function [] = save_fig_png(filename)
 % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % 
 % This function saves a figure both at a matlab .fig file and a .png file
 % so that you don't have to do it manually cause that's annoying when you
-% have a million figures. The .fig files end up in a folder in the current
-% folder called "figs" and the .png files end up in a folder in the main
-% folder called "pngs".
+% have a million figures. The .fig files end up in a folder in the Plots
+% folder called "Figs", the .png files end up in a folder in the Plots
+% folder called "PNGs", and the .pdf files end up in a folder in the Plots 
+% folder called "PDFs".
 % 
 % INPUT: string with the filename you want your files to have
 % 
