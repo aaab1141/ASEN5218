025574f63b5badd7559c7fad3315ff44a71ba8d8
aaab1141 <aaron.aboaf@gmail.com>
2020-04-23 15:03:00 -0600

    command line texts

--- HW6_mainshell.m
+++ HW6_mainshell.m
@@ -22,8 +22,24 @@ bars = [3 6;3 7;2 5;2 6;
         8 7;7 6;6 5;5 8];
     
 plotmytruss(nodes,bars,'Structure 1','m')
+save_fig_png('Q1.Structure 1')
 
-% +pi/4 structure
+% Get the equilibrium matrix and do the svd
+A = equilibrium_matrix(nodes,bars);
+[u,~,w] = svd(A);
+r = rank(A);
+m = size(u,2)-r;
+s = size(w',1)-r;
+disp('Structure: alpha = -pi/4')
+disp(['Rank = ',num2str(r),' | m = ',num2str(m),' | s = ',num2str(s)])
+
+stresses = w(:,end);
+highlightmytruss(nodes,bars,'Structure 1 Bar Stresses','m',stresses)
+
+% Build G and D matrices to test the kind of mechanism we have
+
+
+%% +pi/4 structure
 a = pi/4;
 nodes = [.5  .5  0 1 1 1;
          -.5 .5  0 1 1 1;
@@ -38,5 +54,65 @@ bars = [3 6;3 7;2 5;2 6;
         8 7;7 6;6 5;5 8];
     
 plotmytruss(nodes,bars,'Structure 2','m')
+save_fig_png('Q1.Structure 2')
+
+% Get the equilibrium matrix and do the svd
+A = equilibrium_matrix(nodes,bars);
+[u,~,w] = svd(A);
+r = rank(A);
+m = size(u,2)-r;
+s = size(w',1)-r;
+disp('Structure: alpha = +pi/4')
+disp(['Rank = ',num2str(r),' | m = ',num2str(m),' | s = ',num2str(s)])
+
+stresses = w(:,end);
+highlightmytruss(nodes,bars,'Structure 2 Bar Stresses','m',stresses)
 
 
+
+%% Functions
+function [] = highlightmytruss(nodes,bars,plottitle,units,stresses)
+% Create a figure to work on
+figure
+
+% turn the hold on so that we can plot each bar individually
+hold on
+
+% loop through the bars and plot them all
+for i = 1:size(bars,1)
+    % barnum = i;
+    firstnodenum = bars(i,1);
+    secondnodenum = bars(i,2);
+    x = [nodes(firstnodenum,1);nodes(secondnodenum,1)];
+    y = [nodes(firstnodenum,2);nodes(secondnodenum,2)];
+    z = [nodes(firstnodenum,3);nodes(secondnodenum,3)];
+    if stresses(i) > 0
+        plot3(x,y,z,'r') %plot cable elements in tension red
+    else
+        plot3(x,y,z,'k') %plot solid elements in compression black
+    end
+    
+    % Add the bar number to the bar
+    % find the middle of the bar
+    midx = mean(x);
+    midy = mean(y);
+    midz = mean(z);
+    
+    % Add the text on the plot
+    text(midx,midy,midz,num2str(i),'fontsize',10,'fontangle','italic','color','b')
+end
+
+% Add the node numbers
+for i = 1:size(nodes,1)
+    text(nodes(i,1),nodes(i,2),nodes(i,3),num2str(i),'fontsize',12,'fontweight','bold','color','k')
+end
+
+% Label the plot and change the viewing angle
+title(plottitle)
+xlabel(['X, ',units])
+ylabel(['Y, ',units])
+zlabel(['Z, ',units])
+view(45,35.264)
+axis equal
+grid on
+end
